cdc23e2b2f679fe74a15e830306154ef759fbffe
black-heart-sketch <tchouanana74@gmail.com>
2025-10-15 20:13:57 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["github-snapshow.onrender.com"],
   },
   preview: {
     allowedHosts: ["github-snapshow.onrender.com"],
